c53e2f122a6d2cf7f4cef1c6e77095f097ca2eec
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-07-21 16:44:26 +0900

    next google font

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Noto_Sans_KR, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const noto_sans_kr = Noto_Sans_KR({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
