edc24aac713fe26b67f8a473b62495e5bcfb12fe
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-07-25 16:04:22 +0900

    부분 사전 렌더링

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental', // Partial Prerendering
+  },
+};
 
 export default nextConfig;
