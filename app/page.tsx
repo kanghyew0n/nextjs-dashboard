1418dc4e716a36a56f18e9991d3207d56d8d088f
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-07-30 20:22:12 +0900

    마무리

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
   return (
